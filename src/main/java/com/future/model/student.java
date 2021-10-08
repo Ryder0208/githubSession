dea25c747734b2b745aa28816fd3ea34dc4bafca
Vitthal Anandrao Ghuge <vitthalg123@gmail.com>
2021-10-08 13:26:47 +0530

    IES-06:updated done

--- src/main/java/com/future/model/student.java
+++ src/main/java/com/future/model/student.java
@@ -1,22 +1,37 @@
 package com.future.model;
 
+import java.util.Scanner;
+
 public class student implements studentservice {
+String name;
+
 	
+	public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
 	public void show()
 	{
-		
+		System.out.println("Enter Details of Student");
 	}
 
 	@Override
 	public void get() {
-		// TODO Auto-generated method stub
 		
+		Scanner sc=new Scanner(System.in);
+	
+		System.out.println("enter your name here:");
+		String name=sc.next();
 	}
 
 	@Override
 	public void put() {
-		// TODO Auto-generated method stub
-		
+
+		System.out.println("nothing to do right now");
 	}
 
 }
