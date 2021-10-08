2c6a8f695fc6b0293a795f000e7b4e147dbe1dd4
Vitthal Anandrao Ghuge <vitthalg123@gmail.com>
2021-10-08 13:17:28 +0530

    IES-05:methods added in student class

--- src/main/java/com/future/model/student.java
+++ src/main/java/com/future/model/student.java
@@ -1,9 +1,21 @@
 package com.future.model;
 
-public class student {
+public class student implements studentservice {
 	
 	public void show()
 	{
+		
+	}
+
+	@Override
+	public void get() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void put() {
+		// TODO Auto-generated method stub
 		
 	}
 
