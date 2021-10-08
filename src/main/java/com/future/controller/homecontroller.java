3f933d1553b4811a79b42f6783a16bee3bb318b1
Vitthal Anandrao Ghuge <vitthalg123@gmail.com>
2021-10-08 16:58:09 +0530

    IES-05 :method show added

--- src/main/java/com/future/controller/homecontroller.java
+++ src/main/java/com/future/controller/homecontroller.java
@@ -2,10 +2,17 @@ package com.future.controller;
 
 public class homecontroller {
 	
+
 	
 	public void get()
 	{
 		System.out.println("revoke called");
+	}
+
+	public void show()
+	{
+		System.out.println("hiii");
+
 	}
 
 }
